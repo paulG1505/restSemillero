bc5d470b8556437e6e8cebb62c36b1ae6c6c7ca0
Paul Guaman <guaman.bryan.paul@gmail.com>
2020-12-09 13:09:10 -0500

    metodo insertar

--- src/main/java/com/clearminds/servicios/ServicioPersona.java
+++ src/main/java/com/clearminds/servicios/ServicioPersona.java
@@ -4,39 +4,49 @@ package com.clearminds.servicios;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-import com.clearminds.entidades.Persona;
+import com.clearminds.pg.dtos.Estudiante;
+import com.clearminds.pg.excepciones.BDDExcepcion;
+import com.clearminds.pg.servicios.ServicioEstudiante;
 
-@Path("/srvpersonas")
+@Path("/estudiantes")
 public class ServicioPersona {
-	@Path("/busqueda")
-	@GET
-	public String consultar() {
-		return "Persona";
-	}
-	@Path("/recuperar")
-	@GET
-	@Produces({MediaType.APPLICATION_JSON})//retorno
-	public Persona recuperar() {
-		return new Persona("Paul","Guaman");
-	}
-	@Path("/ingresar")
+//	@Path("/busqueda")
+//	@GET
+//	public String consultar() {
+//		return "Persona";
+//	}
+//	@Path("/recuperar")
+//	@GET
+//	@Produces({MediaType.APPLICATION_JSON})//retorno
+//	public Estudiante recuperar() {
+//		return new Estudiante("Paul","Guaman");
+//	}
+	@Path("/insertar")
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON})//envio
-	public void ingresar(Persona persona){
-		System.out.println("Ingresando Persona"+persona);
+	public void ingresar(Estudiante persona){
+		ServicioEstudiante srvEstudiante= new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(persona)
+			;
+		} catch (BDDExcepcion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			e.getMessage();
+		}
 	}
 	
-	@Path("/actualizar")
-	@PUT
-	@Consumes({MediaType.APPLICATION_JSON})
-	@Produces({MediaType.APPLICATION_JSON})
-	public Persona actualizar(Persona persona){
-		System.out.println("Ingresando Persona"+persona);
-		persona.setName(persona.getName().toUpperCase());
-		persona.setLastName(persona.getLastName().toUpperCase());
-		System.out.println("Persona Modificada"+persona);
-		return persona;
-	}
+//	@Path("/actualizar")
+//	@PUT
+//	@Consumes({MediaType.APPLICATION_JSON})
+//	@Produces({MediaType.APPLICATION_JSON})
+//	public Estudiante actualizar(Estudiante persona){
+//		System.out.println("Ingresando Persona"+persona);
+//		persona.setNombre(persona.getNombre().toUpperCase());
+//		persona.setApellido(persona.getApellido().toUpperCase());
+//		System.out.println("Persona Modificada"+persona);
+//		return persona;
+//	}
 	
 	
 }
